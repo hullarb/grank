175ddb5f7372d6dabe3a883c1f73527128ff29c7
Bela Hullar <hullarb@gmail.com>
2019-05-08 22:17:24 +0200

    lsrepo: Fetch repos from earlier results too

--- lsrepo/main.go
+++ lsrepo/main.go
@@ -14,6 +14,7 @@ import (
 
 var (
 	all     int
+	found   map[string]struct{}
 	client  *github.Client
 	jsonEnc *json.Encoder
 	out     *os.File
@@ -26,10 +27,11 @@ func main() {
 		fmt.Println("GH_TOKEN env var has to contain a valid github api access token")
 		os.Exit(1)
 	}
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: ./lsrepo out_file_name")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./lsrepo out_file_name [older repos json files]")
 		fmt.Println()
 		fmt.Println("out_file_name: a file with the name will be created with a json array of the fetched github repos")
+		fmt.Println("older repos json files: results of earlier runs to ensure that all the repositoreis from those files are fetched")
 		os.Exit(1)
 	}
 	ts := oauth2.StaticTokenSource(
@@ -38,6 +40,7 @@ func main() {
 	ctx := context.Background()
 	tc := oauth2.NewClient(ctx, ts)
 	client = github.NewClient(tc)
+	found = map[string]struct{}{}
 
 	var err error
 	out, err = os.Create(os.Args[1])
@@ -63,9 +66,57 @@ func main() {
 		}
 		lastStars = l
 	}
+	log.Printf("fetched %d repos from API", all)
+	fetchMissing(os.Args[2:])
 	_, err = out.WriteString("]")
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func fetchMissing(repoFiles []string) {
+	for _, f := range repoFiles {
+		inp, err := os.Open(f)
+		if err != nil {
+			log.Fatalf("failed to open: %v", err)
+		}
+		var rs []github.Repository
+		err = json.NewDecoder(inp).Decode(&rs)
+		if err != nil {
+			log.Fatalf("failed to decode: %v", err)
+		}
+		for _, r := range rs {
+			if _, ok := found[r.GetFullName()]; ok {
+				continue
+			}
+			log.Printf("fetching missing: %s", r.GetFullName())
+			for {
+				rp, resp, err := client.Repositories.Get(context.Background(), r.GetOwner().GetLogin(), r.GetName())
+				if err != nil {
+					if _, ok := err.(*github.RateLimitError); ok {
+						log.Println("hit rate limit")
+						time.Sleep(10 * time.Second)
+						continue
+					} else {
+						log.Fatalf("failed to fetch %s: %v", r.GetFullName(), err)
+					}
+				}
+				if _, err = out.Write([]byte{',', '\n'}); err != nil {
+					log.Fatal(err)
+				}
+				if err = jsonEnc.Encode(rp); err != nil {
+					log.Fatal(err)
+				}
+				found[rp.GetFullName()] = struct{}{}
+				if resp.Rate.Remaining == 0 {
+					s := resp.Rate.Reset.Time.Sub(time.Now())
+					log.Printf("quota exhausted, sleeping %v", s)
+					time.Sleep(s)
+				}
+				all++
+				break
+			}
+		}
 	}
 }
 
@@ -103,6 +154,7 @@ func getAllPages(q string) int {
 			}
 			all++
 			last = r.GetStargazersCount()
+			found[r.GetFullName()] = struct{}{}
 		}
 		log.Printf("all: %d, last star count: %v, rate: %v", all, last, resp.Rate)
 		if resp.NextPage == 0 {
